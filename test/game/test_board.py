f2b951e299e035308133c8a1d4ef32a730d06f34
ANDRES FLORIAN <DUVANFLORIANSALAZAR@GMAIL.COM>
2025-04-09 18:42:27 -0500

    Actualizar

--- test/game/test_board.py
+++ test/game/test_board.py
@@ -100,30 +100,86 @@ def test_posicion_deberia_mover_abajo(pos_inicial:tuple, posicion_final:tuple):
         ((-5, 5), (-6, 5) ),   #10
     ]
 )
-def test_posicion_deberia_mover_diagonal_arriba(pos_inicial:tuple, posicion_final:tuple):
+def test_posicion_deberia_mover_diagonal_arriba_derecha(pos_inicial:tuple, posicion_final:tuple):
     p: Posicion = Posicion(fila=pos_inicial[0], columna=pos_inicial[1])
-    resultado: Posicion = p.new_posicion_diagonal_arriba()
+    resultado: Posicion = p.new_posicion_diagonal_arriba_derecha()
     esperado: Posicion = Posicion(fila=posicion_final[0], columna=posicion_final[1])
     
     assert resultado == esperado
+@pytest.mark.parametrize(
+    "pos_inicial,posicion_final",
+    [   ((0, 0), (1, 0) ),  #1
+        ((0, -5), (1, -5) ), #2
+        ((0, 100), (1, 100) ),#3
+        ((-5, 0), (-4, 0) ),  #4
+        ((-5, -5), (-4, -5) ),#5
+        ((-5, 100), (-4, 100) ),#6
+        ((10, 10), (11, 10) ), #7
+        ((-10, -10), (-9, -10) ),#8
+        ((5, -5), (6, -5) ),   #9
+        ((-5, 5), (-4, 5) ),   #10
+    ]
+)
+
+def test_posicion_deberia_mover_diagonal_arriba_izquierda(pos_inicial:tuple, posicion_final:tuple):
+    p: Posicion = Posicion(fila=pos_inicial[0], columna=pos_inicial[1])
+    resultado: Posicion = p.new_posicion_diagonal_arriba_izquierda()
+    esperado: Posicion = Posicion(fila=posicion_final[0], columna=posicion_final[1])
+    
+    assert resultado == esperado
+@pytest.mark.parametrize(
+    "pos_inicial,posicion_final",
+    [   ((0, 0), (1, -1)),      #1 
+        ((0, -5), (1, -6)),    #2 
+        ((0, 100), (1, 99)),  #3
+        ((-5, 0), (-4, -1)),    #4
+        ((-5, -5), (-4, -6)),  #5
+        ((-5, 100), (-4, 99)),#6
+        ((10, 10), (11, 9)),  #7
+        ((-10, -10), (-9, -11)),#8
+        ((5, -5), (6, -6)),    #9
+        ((-5, 5), (-4, 4)),    #10
+    ]
+)
+def test_posicion_deberia_mover_diagonal_abajo_derecha(pos_inicial:tuple, posicion_final:tuple):
+    p: Posicion = Posicion(fila=pos_inicial[0], columna=pos_inicial[1])
+    resultado: Posicion = p.new_posicion_diagonal_abajo_derecha()
+    esperado: Posicion = Posicion(fila=posicion_final[0], columna=posicion_final[1])
+    
+    assert resultado == esperado
+@pytest.mark.parametrize(
+    "pos_inicial,posicion_final",
+    [   ((0, 0), (1, 1)),      #1 
+        ((0, -5), (1, -4)),    #2 
+        ((0, 100), (1, 101)),  #3
+        ((-5, 0), (-4, 1)),    #4
+        ((-5, -5), (-4, -4)),  #5
+        ((-5, 100), (-4, 101)),#6
+        ((10, 10), (11, 11)),  #7
+        ((-10, -10), (-9, -9)),#8
+        ((5, -5), (6, -4)),    #9
+        ((-5, 5), (-4, 6)),    #10
+    ]
+)
 
 @pytest.mark.parametrize(
     "pos_inicial,posicion_final",
-    [   ((0, 0), (1, -1) ),  #1
-        ((0, -5), (1, -6) ),  #2
-        ((0, 100), (1, 99) ), #3
-        ((-5, 0), (-4, -1) ), #4
-        ((-5, -5), (-4, -6) ),#5
-        ((-5, 100), (-4, 99) ),#6
-        ((10, 10), (11, 9) ),  #7
-        ((-10, -10), (-9, -11) ),#8
-        ((5, -5), (6, -6) ),   #9
-        ((-5, 5), (-4, 4) ),   #10
+    [   ((0, 0), (1, 0)),      #1 
+        ((0, -5), (1, -5)),    #2 
+        ((0, 100), (1, 100)),  #3
+        ((-5, 0), (-4, 0)),    #4
+        ((-5, -5), (-4, -5)),  #5
+        ((-5, 100), (-4, 100)),#6
+        ((10, 10), (11, 10)),  #7
+        ((-10, -10), (-9, -10)),#8
+        ((5, -5), (6, -5)),    #9
+        ((-5, 5), (-4, 5)),    #10
     ]
 )
-def test_posicion_deberia_mover_diagonal_abajo(pos_inicial:tuple, posicion_final:tuple):
+
+def test_posicion_deberia_mover_diagonal_abajo_izquierda(pos_inicial:tuple, posicion_final:tuple):
     p: Posicion = Posicion(fila=pos_inicial[0], columna=pos_inicial[1])
-    resultado: Posicion = p.new_posicion_diagonal_abajo()
+    resultado: Posicion = p.new_posicion_diagonal_abajo_izquierda()
     esperado: Posicion = Posicion(fila=posicion_final[0], columna=posicion_final[1])
     
     assert resultado == esperado
