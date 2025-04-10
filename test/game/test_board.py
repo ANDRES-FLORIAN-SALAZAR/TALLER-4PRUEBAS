a9f137d400e7d2a87d8afafaa057230f6dc8b392
ANDRES FLORIAN <DUVANFLORIANSALAZAR@GMAIL.COM>
2025-04-09 20:05:26 -0500

    ACTUALIZAR

--- test/game/test_board.py
+++ test/game/test_board.py
@@ -1,5 +1,6 @@
 import pytest
-from lucesfuera.game.board import Posicion
+from lucesfuera.game.board import Posicion, Tablero
+from lucesfuera.game.luz import Luz
 
 @pytest.mark.parametrize(
     "pos_inicial,posicion_final",
@@ -162,24 +163,80 @@ def test_posicion_deberia_mover_diagonal_abajo_derecha(pos_inicial:tuple, posici
     ]
 )
 
-@pytest.mark.parametrize(
-    "pos_inicial,posicion_final",
-    [   ((0, 0), (1, 0)),      #1 
-        ((0, -5), (1, -5)),    #2 
-        ((0, 100), (1, 100)),  #3
-        ((-5, 0), (-4, 0)),    #4
-        ((-5, -5), (-4, -5)),  #5
-        ((-5, 100), (-4, 100)),#6
-        ((10, 10), (11, 10)),  #7
-        ((-10, -10), (-9, -10)),#8
-        ((5, -5), (6, -5)),    #9
-        ((-5, 5), (-4, 5)),    #10
-    ]
-)
-
 def test_posicion_deberia_mover_diagonal_abajo_izquierda(pos_inicial:tuple, posicion_final:tuple):
     p: Posicion = Posicion(fila=pos_inicial[0], columna=pos_inicial[1])
     resultado: Posicion = p.new_posicion_diagonal_abajo_izquierda()
     esperado: Posicion = Posicion(fila=posicion_final[0], columna=posicion_final[1])
     
     assert resultado == esperado
+
+@pytest.mark.parametrize(
+    "Tablero_esperado, ancho, alto",
+    [   ([[Luz.OFF]], 6, 6), #1
+        ([[Luz.OFF, Luz.OFF]], 2, 1), #2
+    ],
+)
+
+def test_tablero_creacion_vacio(tablero_esperado,ancho,alto):
+    resultado = Tablero(ancho,alto).Tablero
+    assert tablero_esperado == resultado
+
+@pytest.fixture
+def tablero_grande():
+    return Tablero(10, 10)
+
+@pytest.fixture
+def tablero_manual():
+    return[
+    [Luz.OFF,Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF],
+    [Luz.OFF,Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF],
+    [Luz.OFF,Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF],
+    [Luz.OFF,Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF],
+    [Luz.OFF,Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF],
+    [Luz.OFF,Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF],
+    [Luz.OFF,Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF],
+    [Luz.OFF,Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF],
+    [Luz.OFF,Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF],
+    [Luz.OFF,Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF],
+    ]
+
+@pytest.mark.parametrize(
+    "tablero_manual,tablero_grande",
+    ([[Luz.OFF,Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF],
+    [Luz.OFF,Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF],
+    [Luz.OFF,Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF],
+    [Luz.OFF,Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF],
+    [Luz.OFF,Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF],
+    [Luz.OFF,Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF],
+    [Luz.OFF,Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF],
+    [Luz.OFF,Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF],
+    [Luz.OFF,Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF],
+    [Luz.OFF,Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF],
+    ]
+))
+
+def test_tablero_prender_apagar(tablero_grande, tablero_manual):
+    tablero_grande.prender_apagar= (Posicion(0, 0))
+    tablero_manual = [0][0] = Luz.ON
+    assert tablero_grande.tablero == tablero_manual
+    
+    tablero_grande.prender_apagar= (Posicion(0, 0))
+    tablero_manual = [0][0] = Luz.OFF
+    assert tablero_grande.tablero[0][0] == tablero_manual
+    
+    tablero_grande.prender_apagar= (Posicion(9, 9))
+    tablero_manual = [9][9] = Luz.ON
+    assert tablero_grande.tablero == tablero_manual
+    
+    tablero_grande.prender_apagar= (Posicion(9, 9))
+    tablero_manual = [9][9] = Luz.OFF
+    assert tablero_grande.tablero[9][9] == tablero_manual
+
+@pytest.mark.parametrize    
+def test_tablero_switch_luz(tablero_grande,tablero_manual):
+    tablero_grande.switch_luz= (Posicion(0, 0))
+    tablero_manual = [0][0] = Luz.ON
+    tablero_manual = [0][1] = Luz.ON
+    tablero_manual = [1][0] = Luz.ON
+    tablero_manual = [1][1] = Luz.ON
+    assert tablero_grande.tablero == tablero_manual
---
+++ test/game/test_tablero.py
@@ -0,0 +1,3 @@
+from lucesfuera.game import Tablero
+
+t= Tablero(withth=6, height=6)
